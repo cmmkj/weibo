c412221babb55978e9c3d6ac4e2c7384d1bd8681
along <1594466572@qq.com>
2017-09-28 18:17:51 +0800

    fix

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,10 @@
 const passport = require('../lib/passport')
 
 function* weiboSuccess (next) {
+  console.log('nnnnnnnnnnnnnnnnn')
+  console.log('nnnnnnnnnnnnnnnnn')
+  console.log('nnnnnnnnnnnnnnnnn')
+  console.log('nnnnnnnnnnnnnnnnn')
   this.body = '恭喜你,微博授权成功'
 }
 module.exports = {
